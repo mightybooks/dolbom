c58a0f97915f8a33c99674c95d8c5a81e0b7d8b4
mightybooks <novelstudylab@naver.com>
2025-12-12 15:20:31 +0900

    tenth

--- app/result/layout.tsx
+++ app/result/layout.tsx
@@ -1,9 +1,12 @@
-/** @jsxImportSource react */
 "use client";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export default function ResultLayout({ children }: { children: React.ReactNode }) {
+export default function ResultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <div>{children}</div>;
 }
